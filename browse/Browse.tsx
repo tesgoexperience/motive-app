089e912640db7af75a4055406b618f5014460cf0
Tesgo exp <abdiidris14@gmail.com>
2022-12-11 01:35:41 +0000

    browse different views

--- browse/Browse.tsx
+++ browse/Browse.tsx
@@ -1,49 +1,77 @@
 import React, { Component } from 'react';
 
 import { ScrollView, View, Text, TouchableOpacity, RefreshControl } from 'react-native';
-import { randUserName, randProductDescription, randJobTitle, randNumber, randImg } from '@ngneat/falso';
 import EventCard, { Motive } from './EventCard';
 import Api from '../util/Api';
-import { plainToInstance } from 'class-transformer';
+import {Loading} from '../util/Loading'
 
+enum VIEW {
+    ALL = 'all',
+    ATTENDING = 'attending',
+    FINISHED = 'finished'
+}
 
+type Stats = {
+    attending: number,
+    finished: number,
+    all: number
+}
 type StateType = {
     refreshingViaPulldown: boolean,
-    motiveList: Array<Motive>
+    motives: Array<Motive>,
+    view: VIEW,
+    loading: boolean,
+    stats: Stats | null
 }
 
 class Browse extends Component<{}, StateType>{
-    
-    state: StateType = { refreshingViaPulldown: false, motiveList: [] }
-    
+
+    state: StateType = { refreshingViaPulldown: false, motives: [], view: VIEW.ALL, loading: true, stats: null}
+
     componentDidMount() {
-        this.getMotiveList();
+        this.getMotiveList(VIEW.ALL);
     }
 
-    getMotiveList() {
-        let motives : Array<Motive> ;
-        Api.get('/motive/').then((res) => {
-            this.setState({ motiveList: res.data });
+    getMotiveList(view: VIEW) {
+        this.setState({ loading: true,motives: []});
+        Api.get('/motive/'+view).then((res) => {
+            this.setState({ motives: res.data });
+
+            // get the stats also
+            Api.get('/motive/stats').then((statsRes) => {
+                this.setState({ stats: statsRes.data });
+                this.setState({ loading: false });
+            });
+
         });
-        
-    
-       
+
     }
+
+    changeView(view : VIEW) {
+        this.setState({view:view});
+        this.getMotiveList(view);
+    }
+
     render() {
 
-        let motives = this.state.motiveList.map(motive => { return <EventCard key={motive.id} motive={motive} /> })
-        
+        if (this.state.loading) {
+            return <Loading />
+        }
+
+        let motives = this.state.motives.map(motive => { return <EventCard key={motive.id} motive={motive} /> })
+
         return <ScrollView refreshControl={<RefreshControl refreshing={this.state.refreshingViaPulldown} onRefresh={() => {
-                this.setState({ refreshingViaPulldown: true });
-                this.getMotiveList();
-                this.setState({ refreshingViaPulldown: false });
-            }} />} showsVerticalScrollIndicator={false} contentContainerStyle={{ alignItems: 'center', paddingTop:20}}>
-                <View style={{width:'100%', flexDirection: "row", justifyContent: 'space-between'}}>
-                    <TouchableOpacity style={{width:'45%', borderWidth: 1, borderColor: '#69FFAA', borderRadius: 5, padding: 8}}><Text style={{ textAlign: 'center' }}>Not Involved<Text style={{ color: 'red', fontWeight: 'bold' }}> • 13</Text></Text></TouchableOpacity>
-                    <TouchableOpacity style={{ width:'45%',borderWidth: 1, borderColor: '#E2E2E2', borderRadius: 5, padding: 8 }}><Text style={{ textAlign: 'center' }}>Involved<Text style={{ color: 'red', fontWeight: 'bold' }}> • 2</Text></Text></TouchableOpacity>
-                </View>
-                {motives}
-            </ScrollView>
+            this.setState({ refreshingViaPulldown: true });
+            this.getMotiveList(this.state.view);
+            this.setState({ refreshingViaPulldown: false });
+        }} />} showsVerticalScrollIndicator={false} contentContainerStyle={{ alignItems: 'center', paddingTop: 20 }}>
+            <View style={{ width: '100%', flexDirection: "row", justifyContent: 'space-between' }}>
+                <TouchableOpacity onPress={()=>this.changeView(VIEW.ALL)} style={{ width: '30%', borderWidth: 1, borderColor: '#69FFAA', borderRadius: 5, padding: 8 }}><Text style={{ textAlign: 'center' }}>All<Text style={{ color: 'red', fontWeight: 'bold' }}> • {this.state.stats?.all}</Text></Text></TouchableOpacity>
+                <TouchableOpacity onPress={()=>this.changeView(VIEW.ATTENDING)} style={{ width: '30%', borderWidth: 1, borderColor: '#E2E2E2', borderRadius: 5, padding: 8 }}><Text style={{ textAlign: 'center' }}>Attending<Text style={{ color: 'red', fontWeight: 'bold' }}> • {this.state.stats?.attending}</Text></Text></TouchableOpacity>
+                <TouchableOpacity style={{ width: '30%', borderWidth: 1, borderColor: '#69FFAA', borderRadius: 5, padding: 8 }}><Text style={{ textAlign: 'center' }}>Finished<Text style={{ color: 'red', fontWeight: 'bold' }}> • 13</Text></Text></TouchableOpacity>
+            </View>
+            {motives}
+        </ScrollView>
     }
 
 }
